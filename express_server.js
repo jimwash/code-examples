d12073784a3d4b827529ba515f30c9a6305f24eb
jim washburn <jimwash@yahoo.com>
2017-02-02 20:42:14 +0000

    other part of rename

---
+++ express_server.js
@@ -0,0 +1,26 @@
+var express = require('express');;
+var app = express();
+
+app.set('views',".");
+app.set('view engine', 'jade');
+
+var path    = require("path");
+
+app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/node_modules'));
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html');
+});
+
+app.get('/promise', function (req, res) {
+  res.sendFile(__dirname+'/index2.html');
+});
+
+app.get('/backbone',function (req, res) {
+  res.sendFile(__dirname+'/index3.html');
+})
+
+app.listen(3000, function () {
+  console.log('Express server listening on port 3000!');
+});
