7148e4fad9c67d3de1f93de6fef35218f93e9845
jim washburn <jimwash@yahoo.com>
2017-02-02 22:34:36 +0000

    from running jshint

--- coupon.js
+++ coupon.js
@@ -1,45 +1,48 @@
 var CartItem = function(name, price, type) {
 	this.price = price;
 	this.type = type;
-	this.coupons = []
-}
+	this.coupons = [];
+};
 
 var Coupon = function(type, amount, itemtype, n) {
 	this.type = type;      /* P, N, D */
 	this.amount = amount;
 	this.itemtype = itemtype;
 	this.n = n;
-}
+};
 
 var Cart = function(items) {
    this.items = items;
-}
+};
 
 Cart.prototype.applyCoupons = function() {
 	var self = this;
 	this.items.forEach(function(elem,idx) {
+        var cartitem;
+        var i;
         if (elem instanceof Coupon) {
         	if (elem.type == "P") { 
-        		self.items.forEach(function (cartitem, cartidx) {
+                for (i=0;i<self.items.length;i++) {
+                    cartitem = self.items[i];
         			if (cartitem instanceof CartItem) {
-        				cartitem.coupons.push(elem)
+        				cartitem.coupons.push(elem);
         			}
 
-        		})
+        		}              
         	} else if (elem.type == "N") {
-        		for (var i=idx;i<self.items.length;i++) {
-        			var cartitem = self.items[i];
+        		for (i=idx;i<self.items.length;i++) {
+        		    cartitem = self.items[i];
         			if (cartitem instanceof CartItem) {
         				cartitem.coupons.push(elem);
         				break;
         			}
         		}
         	} else {
-                for (var i=0;i<self.items.length;i++) {
+                for (i=0;i<self.items.length;i++) {
                 	var typecount = 0;
-                	var cartitem = self.items[i];
+                	cartitem = self.items[i];
                 	if (cartitem instanceof CartItem) {
-                        if (cartitem.type = elem.coupontype) {
+                        if (cartitem.type == elem.coupontype) {
                         	typecount++;
                         	if (elem.n == typecount) {
                         		cartitem.coupons.push(elem);
@@ -50,8 +53,8 @@ Cart.prototype.applyCoupons = function() {
                 }
         	}
         }
-	})
-}
+	});
+};
 
 Cart.prototype.total = function() {
 	var total = 0;
@@ -61,7 +64,7 @@ Cart.prototype.total = function() {
 			var price = item.price;
 			for (var j=0;j<item.coupons.length;j++) {
 				var coupon = item.coupons[j];
-				price = price - (price * coupon.amount)
+				price = price - (price * coupon.amount);
 			}
 
 			total = total+price;
@@ -69,12 +72,12 @@ Cart.prototype.total = function() {
 	}
 
 	console.log("Total:"+total);
-}
+};
 
 var items = [new Coupon("N",".05"),new CartItem("9 inch Pan",19.99),
              new Coupon("A",4.0,"Planter",2), new CartItem("Planter",10.00), 
-             new Coupon("P","0.10"), new Coupon("Planter",10.00)]
+             new Coupon("P","0.10"), new Coupon("Planter",10.00)];
              
-var cart = new Cart(items)
+var cart = new Cart(items);
 cart.applyCoupons();
 cart.total();
